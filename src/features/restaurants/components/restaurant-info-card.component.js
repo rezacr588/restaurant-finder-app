b8ab593db527140b8803c78981a8c31ea4f1f78c
Reza Zeraat <rezacr588@gmail.com>
2021-08-05 00:28:38 +0430

    add flat list feature

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,51 +1,20 @@
 import React from "react"
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg"
 import starSVG from "../../../../assets/star";
 import openSVG from "../../../../assets/open";
 import { Spacer } from "../../../components/spacer/spacer.component"
+import { Text } from "../../../components/typography/text.component";
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
 
-const Title = styled.Text`
-  font-family: ${props => props.theme.fonts.heading};
-  font-size: ${props => props.theme.fontSizes.title};
-  color: ${props => props.theme.colors.ui.primary};
-`;
-
-const Address = styled.Text`
-  font-family: ${props => props.theme.fonts.body};
-  font-size: ${props => props.theme.fontSizes.caption};
-`;
-
-const Info = styled.View`
-  padding: ${props => props.theme.space[3]};
-`
-
-const CardWrapper = styled(Card)`
-  background-color: white;
-`
-
-const CardCover = styled(Card.Cover)`
-  padding: ${props => props.theme.space[3]};
-  background-color: white;
-`
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${props => props.theme.space[3]};
-  padding-bottom: ${props => props.theme.space[3]};
-`
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`
 
 export const RestaurantsInfoCard = ({ restaurant = {} }) => {
   const {
@@ -62,29 +31,29 @@ export const RestaurantsInfoCard = ({ restaurant = {} }) => {
 
   const ratingArray = Array.from(new Array(Math.floor(rating)))
 
-  return <CardWrapper elevation={5}>
-    <CardCover key={name} source={{ uri: photos[0] }} />
-    <Info>
-      <Title>{name}</Title>
-      <Section>
-        <Rating>
-          {ratingArray.map(() => <SvgXml xml={starSVG} height={20} width={20} />)}
-        </Rating>
-        <SectionEnd>
-          <Spacer size="medium" position="left">
-            {isClosedTemprorarly && <Text variant="label" style={{ color: "red" }}>
-              CLOSED TEMPORARILY
-            </Text>}
-          </Spacer>
-          <Spacer size="medium" position="left">
-            {isOpenNow && <SvgXml height={20} width={20} xml={openSVG} />}
-          </Spacer>
-          <Spacer size="medium" position="left">
-            <Image style={{ width: 15 , height: 15 }} source={{ uri: icon }} />
-          </Spacer>
-        </SectionEnd>
-      </Section>
-      <Address>{address}</Address>
-    </Info>
-  </CardWrapper>;
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => <SvgXml xml={starSVG} height={20} width={20} />)}
+          </Rating>
+          <SectionEnd>
+            <Spacer size="medium" position="left">
+              {isClosedTemprorarly && <Text variant="error">CLOSED TEMPORARILY</Text>}
+            </Spacer>
+            <Spacer size="medium" position="left">
+              {isOpenNow && <SvgXml height={20} width={20} xml={openSVG} />}
+            </Spacer>
+            <Spacer size="medium" position="left">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
 };
