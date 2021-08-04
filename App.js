bf3a8e743260d550022e3f48b6c719fc22ae7174
Reza Zeraat <rezacr588@gmail.com>
2021-08-05 00:38:41 +0430

    add-bottom-navigator

--- App.js
+++ App.js
@@ -6,6 +6,39 @@ import { theme } from "./src/infrastructure"
 import AppLoading from 'expo-app-loading';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {Text} from "react-native"
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeArea } from './src/components/utility/safe-area.component'
+
+const Tab = createBottomTabNavigator()
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+)
+
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+)
+
+const Screens = [
+  {
+    name: "map",
+    component: Map
+  },
+  {
+    name: "settings",
+    component: Settings
+  },
+  {
+    name: "restaurants",
+    component: RestaurantsScreen
+  },
+]
 
 export default function App() {
 
@@ -23,6 +56,13 @@ export default function App() {
     return (
       <>
         <ThemeProvider theme={theme} >
+          <NavigationContainer>
+            <Tab.Navigator>
+              {Screens.map(screen => (
+                <Tab.Screen component={screen.component} name={screen.name} />
+              ))}
+            </Tab.Navigator>
+          </NavigationContainer>
           <RestaurantsScreen />
         </ThemeProvider>
         <ExpoStatusBar style="auto" />
