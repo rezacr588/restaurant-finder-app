db05449a6702e5f151fd12525213154fef4fa565
Reza Zeraat <rezacr588@gmail.com>
2021-08-12 14:15:11 +0430

    debug image

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
 import WebView from "react-native-webview";
-import { Platform } from "react-native";
 import { Text } from "../typography/text.component";
 
 const CompactImage = styled.Image`
@@ -22,15 +21,13 @@ const Item = styled.View`
   align-items: center;
 `;
 
-const isAndroid = Platform.OS === "android";
-
-export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactWebview : CompactImage;
+export const CompactRestaurantInfo = ({ restaurant, map = false }) => {
+  const Image = map ? CompactWebview : CompactImage;
   return (
     <Item>
       <Image
-        originWhitelist={['*']}
-        source={{ uri: restaurant.photos[0] }} />
+        source={{ uri: restaurant.photos[0] }}
+      />
       <Text center variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 export const MapCallout = ({ restaurant }) => (
-  <CompactRestaurantInfo restaurant={restaurant} />
+  <CompactRestaurantInfo restaurant={restaurant} map={true} />
 );
