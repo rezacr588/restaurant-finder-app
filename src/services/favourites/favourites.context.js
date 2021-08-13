d853b96d4bc4247691abc4ac100570f6be7c8717
Reza Zeraat <rezacr588@gmail.com>
2021-08-13 14:22:29 +0430

    store favourites on storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
@@ -8,6 +9,32 @@ export const FavouritesContextProvider = ({ children }) => {
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
   };
+
+  const storeFavourirtes = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@favourites', jsonValue)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const getFavourites = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@favourites')
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    setFavourites(() => getFavourites())
+  }, [])
+  
+  useEffect(() => {
+    storeFavourirtes(favourites)
+  },[favourites])
 
   const remove = (restaurant) => {
     const newFavourites = favourites.filter(
