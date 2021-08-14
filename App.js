144192ca75197778693598a5c6b8d319ea462f9c
Reza Zeraat <rezacr588@gmail.com>
2021-08-14 10:17:31 +0430

    install firebase

--- App.js
+++ App.js
@@ -9,6 +9,18 @@ import { RestaurantContextProvider } from './src/services/restaurants/restaurant
 import { LocationContextProvider } from "./src/services/locations/location.context";
 import { Navigation } from "./src/infrastructure/navigation"
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD78j_dckakL36TkI8kpJ0ZMps7OjIS1po",
+  authDomain: "restaurant-19ba0.firebaseapp.com",
+  projectId: "restaurant-19ba0",
+  storageBucket: "restaurant-19ba0.appspot.com",
+  messagingSenderId: "875326891819",
+  appId: "1:875326891819:web:e8a987a061dc5c53b3476e"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
 
