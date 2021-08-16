6202009d495aa936a6628491d803053388cfe433
Reza Zeraat <rezacr588@gmail.com>
2021-08-16 15:16:17 +0430

    add watermelon

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,27 @@
 import React from "react"
-import { AccountBackground, AccountContainer, AccountCover, AuthButton, Title } from "../components/styles.component"
-import {Spacer} from "../../../components/spacer/spacer.component"
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AnimationWrapper,
+  AuthButton,
+  Title
+} from "../components/styles.component"
+import { Spacer } from "../../../components/spacer/spacer.component"
+import LottieView from 'lottie-react-native';
 
 export const AccountScreen = ({navigation}) => (
   <AccountBackground>
     <AccountCover />
+    <AnimationWrapper>
+      <LottieView
+        key="animation"
+        autoPlay
+        loop
+        resizeMode="cover"
+        source={require("../../../../assets/watermelon.json")}
+      />
+    </AnimationWrapper>
     <Title>
       Restaurant
     </Title>
