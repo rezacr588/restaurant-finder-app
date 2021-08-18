c607ece1dad6fab3db29c3834eb8b0a06f897e43
Reza Zeraat <rezacr588@gmail.com>
2021-08-18 10:39:21 +0430

    add camera permissions

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,3 +1,35 @@
-import React from "react"
+import React , {useState, useEffect} from "react"
+import { Camera } from "expo-camera"
+import { useRef } from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
 
-export const CameraScreen = () => null
+const ProfileCamera = styled(Camera)`
+  height: 100%;
+  width: 100%;
+`
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  const cameraRef = useRef()
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return <ProfileCamera
+    ref={(camera) => (cameraRef.current = camera)}
+    type={Camera.Constants.Type.front}></ProfileCamera>
+}
